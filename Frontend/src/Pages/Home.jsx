13583107479d614db98c19b10a6568fc2a8d5844
yasiyaa <yasithvidusara@gmail.com>
2023-09-29 21:27:28 +0530

    Fourth Commit

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -5,10 +5,12 @@ import heroImage03 from "../assets/images/hero-img03.png";
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
-import {BsArrowRight,} from "react-icons/bs"
+import videoImage from "../assets/images/video-icon.png";
+import featureImage from "../assets/images/feature-img.png";
+import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import About from "../Components/About/About";
-
+import ServiceList from "../Components/Servieces/ServiceList";
 
 const Home = () => {
   return (
@@ -116,8 +118,7 @@ const Home = () => {
                   to="/doctors"
                   className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
-                  <BsArrowRight className="group-hover:text-white w-6 h-5"/>
-
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
                 </Link>
               </div>
             </div>
@@ -140,8 +141,7 @@ const Home = () => {
                   to="/services"
                   className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
-                  <BsArrowRight className="group-hover:text-white w-6 h-5"/>
-
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
                 </Link>
               </div>
             </div>
@@ -164,19 +164,93 @@ const Home = () => {
                   to="/contact"
                   className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
                 >
-                  <BsArrowRight className="group-hover:text-white w-6 h-5"/>
-
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
                 </Link>
               </div>
             </div>
           </div>
-
-          
         </div>
       </section>
 
-      <About></About>
-      
+      <About />
+
+      {/*=========Services section ========*/}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our medical services</h2>
+            <p className="text__para text-center">
+              world class care for everyone. Our health system offers
+              unmatched,expert health care
+            </p>
+          </div>
+
+          <ServiceList />
+        </div>
+      </section>
+
+      {/*=========Services section end========*/}
+
+      {/*=========Fetures section ========*/}
+
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/*=========Fetures content section ========*/}
+
+            <div className="xl:w-[670px]">
+              <h2 className="heading">
+                Get virtual treatment <br /> anytime
+              </h2>
+
+              <ul className="pl-4">
+                <li className="text__para">
+                  1.Schedule the appointment directly
+                </li>
+                <li className="text__para">
+                  2.Search for your physician here, and contact their office.
+                </li>
+                <li className="text__para">
+                  3.View our physicians who are accepting new patients, use the
+                  online scheduling tool to select an appointment time.
+                </li>
+              </ul>
+              <Link to="/">
+                <button className="btn">Learn More</button>
+              </Link>
+            </div>
+            {/*=========Fetures Image=======*/}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImage} alt="" className="w-3/4" />
+
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                      Tue, 24
+                    </p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]">
+                      10.00AM
+                    </p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
+                    <img src={videoImage} alt="" />
+                  </span>
+                </div>
+
+
+                <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
+                  Consultation
+                </div>
+
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/*=========Fetures section end========*/}
     </>
   );
 };
