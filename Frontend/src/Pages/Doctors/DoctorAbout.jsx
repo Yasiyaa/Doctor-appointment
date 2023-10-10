01852ffe9ebf6229416436c6183ee13c87c73371
Yasiyaa <yasithvidusara@gmail.com>
2023-10-10 15:37:44 +0530

    ninth commit

--- Frontend/src/Pages/Doctors/DoctorAbout.jsx
+++ Frontend/src/Pages/Doctors/DoctorAbout.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { formatDate } from "../../utils/FormatDate";
+
 
 const DoctorAbout = () => {
   return (
@@ -26,7 +28,9 @@ const DoctorAbout = () => {
         <ul className="pt-4 md:p-5">
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
-              <span className="text-irisBlueColor">23 June, 2008</span>
+              <span className="text-irisBlueColor">
+              {formatDate('12-04-2010')}
+                </span>
               <p className="text-[16px] leading-6 font-medium text-textColor">
                 PHD in Surgeon
               </p>
@@ -38,7 +42,9 @@ const DoctorAbout = () => {
           </li>
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
-              <span className="text-irisBlueColor"></span>
+              <span className="text-irisBlueColor">
+              {formatDate('12-05-2010')}
+              </span>
               <p className="text-[16px] leading-6 font-medium text-textColor">
                 PHD in Surgeon
               </p>
@@ -50,6 +56,47 @@ const DoctorAbout = () => {
           </li>
         </ul>
       </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Experience
+        </h3>
+
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+            {formatDate('12-05-2010')}
+            </span>
+
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+                Sr. Surgeon   
+              </p>
+
+              <p className="text-[16px] leading-6 font-medium text-textColor">
+              New Appolo Hospital, New York.  
+              </p>
+            
+          </li>
+
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+            {formatDate('12-05-2010')}
+            </span>
+
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+                Sr. Surgeon   
+              </p>
+
+              <p className="text-[16px] leading-6 font-medium text-textColor">
+              New Appolo Hospital, New York.  
+              </p>
+            
+          </li>
+
+        </ul>
+      </div>
+
+
     </div>
   );
 };
---
+++ Frontend/src/Pages/Doctors/FeedbackForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FeedbackForm = () => {
+  return (
+    <form action="">
+      <div>
+        <h3 className="text-textColor text-[16px] leading-6 font-semibold mb-4">
+          How would you rate the overall experience ?
+        </h3>
+      </div>
+    </form>
+  );
+};
+
+export default FeedbackForm;
