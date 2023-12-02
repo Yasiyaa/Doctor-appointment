a5a5c0cc0556d1ed5ada2e20d7c5a51fc4d09f0e
Yasiyaa <yasithvidusara@gmail.com>
2023-12-02 14:46:25 +0530

    frontend done

--- Frontend/src/Pages/Contact.jsx
+++ Frontend/src/Pages/Contact.jsx
@@ -1,11 +1,57 @@
-import React from "react";  
+import React from "react";
 
-const Contact = () =>{
-    return(
-        <>
-        contact
-        </>
-    )
-}
+const Contact = () => {
+  return (
+    <section>
+      <div className="px-4 mx-auto max-w-screen-md">
+        <h2 className="heading text-center">Contact us</h2>
+        <p className="mb-8 lg:mb-16 font-light text-center text__para">
+          Got a techincal issue ? Want to send feedback about a beta feature ?
+          Let us know.
+        </p>
+        <form action="" className="space-y-8">
+          <div>
+            <label htmlFor="email" className="form_label">
+              {" "}
+              Your email
+            </label>
+            <input
+              type="email"
+              placeholder="Example@gmail.com"
+              className="form_input mt-1"
+            />
+          </div>
+
+          <div className="sm:col-span-2">
+            <label htmlFor="Subject" className="form_label">
+              {" "}
+             Subject
+            </label>
+            <input
+              type="text"
+              placeholder="Let us know how we can help you"
+              className="form_input mt-1"
+            />
+          </div>
+
+          <div className="sm:col-span-2">
+            <label htmlFor="message" className="form_label">
+              {" "}
+             Your message
+            </label>
+            <textarea
+            rows="6"
+              type="text"
+              placeholder="Leave a comment"
+              className="form_input mt-1"
+            />
+          </div>
+
+          <button type="submit" className="btn rounded sm:w-fit">Submit</button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 import signupImg from "../assets/images/signup.gif";
-import avatar from "../assets/images/doctor-img01.png"
+import avatar from "../assets/images/doctor-img01.png";
 const Signup = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [previewURL, setPriviewURL] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    photo: selectedFile,
+    gender: "",
+    role: "patient",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleFileInputChange = async (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+  };
   return (
     <section>
       <div className="max-w-[1170px] mx-auto">
@@ -19,11 +44,13 @@ const Signup = () => {
               Create an <span className="text-primaryColor">account</span>
             </h3>
 
-            <form>
+            <form onSubmit={submitHandler}>
               <div className="mb-5">
                 <input
                   type="text"
                   placeholder="Full name"
+                  value={formData.name}
+                  onChange={handleInputChange}
                   name="name"
                   className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none
               focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md
@@ -35,6 +62,8 @@ const Signup = () => {
               <div className="mb-5">
                 <input
                   type="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
                   placeholder="Enter your email"
                   name="email"
                   className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none
@@ -48,6 +77,8 @@ const Signup = () => {
                 <input
                   type="password"
                   placeholder="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
                   name="password"
                   className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none
               focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md
@@ -65,6 +96,8 @@ const Signup = () => {
                   <select
                     name="role"
                     id=""
+                    value={formData.role}
+                    onChange={handleInputChange}
                     className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
                   >
                     <option value="patient">Patient</option>
@@ -80,6 +113,8 @@ const Signup = () => {
                   <select
                     name="gender"
                     id=""
+                    value={formData.gender}
+                    onChange={handleInputChange}
                     className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
                   >
                     <option value="Select">Select</option>
@@ -90,17 +125,52 @@ const Signup = () => {
                 </label>
               </div>
 
-            <div className="mb-5 flex items-center gap-3">
+              <div className="mb-5 flex items-center gap-3">
                 <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
-                    <img src={avatar} alt="" className="w-full rounded-full"/>
+                  <img src={avatar} alt="" className="w-full rounded-full" />
                 </figure>
 
-                <div>
-                    <input type="file" name="photo" id="customerFile"
-                    accept=".jpg,.png"/>
-                </div>
-            </div>
+                <div className=" relative w-[130px] h-[50px]">
+                  <input
+                    type="file"
+                    name="photo"
+                    id="customerFile"
+                    accept=".jpg,.png"
+                    onChange={handleFileInputChange}
+                    className="absolute top-0 left-0 h-full opacity-0 cursor-pointer"
+                  />
 
+                  <label
+                    htmlFor="customfile"
+                    className="absolute top-0 left-0 w-full h-full flex 
+                  items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden
+                   bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer"
+                  >
+                    Upload photo
+                  </label>
+                </div>
+              </div>
+
+              <div className="mt-7">
+                <button
+                  type="submit"
+                  className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3"
+                >
+                  Sign up
+                </button>
+              </div>
+
+              <p className="mt-5 text-textColor text-center">
+                Already have an account ?
+                <Link
+                  to="/login"
+                  className="text-primaryColor
+            font-medium ml-1"
+                >
+                  {" "}
+                  Login
+                </Link>
+              </p>
             </form>
           </div>
         </div>
